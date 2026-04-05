4ed1d028b23dbe73c2057af1a40a83cf1a9d3887
Dominic Myers <annoyingmouse@gmail.com>
2026-04-05 03:22:55 +0100

    ♻️ (script.js): refactor icon class for the start button to use a bell icon for better user feedback when time is up

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ function render() {
 
   const startIcon = btnStart.querySelector("i");
   if (remaining <= 0) {
-    startIcon.className = "fa-sharp fa-solid fa-stopwatch fa-shake";
+    startIcon.className = "fa-sharp fa-solid fa-bell fa-shake fa-beat";
   } else {
     startIcon.className = "fa-sharp fa-solid fa-play";
   }
